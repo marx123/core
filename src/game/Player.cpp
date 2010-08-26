5fd3824b29522e317a7f19ce1443e8a02b340af9
Singlem <Singlem@gmail.com>
2010-08-26 16:50:42 +0200

    Some more possible crash fixes

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -605,6 +605,7 @@ Player::Player (WorldSession *session): Unit(), m_achievementMgr(this), m_reputa
 
     m_lastFallTime = 0;
     m_lastFallZ = 0;
+	m_grid_update_timer = 0;
 }
 
 Player::~Player ()
